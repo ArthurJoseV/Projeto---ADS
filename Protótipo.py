b091db6458c9f865d0c0a20e72ab9fd624cdd142
ArthurJoseV <arthurjosevinicius@hotmail.com>
2025-02-04 21:14:56 -0300

    Att de variáveis

--- Protótipo.py
+++ Protótipo.py
@@ -7,10 +7,10 @@ agendamentos = []
 
 # Função para adicionar clientes ao sistema da barbearia
 def adicionar_cliente():
-    nome = add_nome.get()
-    sobrenome = add_sobrenome.get()
-    telefone = add_telefone.get()
-    email = add_email.get()
+    nome = msg_nome.get()
+    sobrenome = msg_sobrenome.get()
+    telefone = msg_telefone.get()
+    email = msg_email.get()
     
     if nome and sobrenome and telefone and email:
         cliente = {
@@ -21,10 +21,10 @@ def adicionar_cliente():
         }
         clientes.append(cliente)
         messagebox.showinfo("Sucesso", f"Cliente {nome} adicionado com sucesso! Seja bem-vindo!")
-        add_nome.delete(0, 'end')
-        add_sobrenome.delete(0, 'end')
-        add_telefone.delete(0, 'end')
-        add_email.delete(0, 'end')
+        msg_nome.delete(0, 'end')
+        msg_sobrenome.delete(0, 'end')
+        msg_telefone.delete(0, 'end')
+        msg_email.delete(0, 'end')
     else:
         messagebox.showwarning("Erro", "Todos os campos devem ser preenchidos!")
 
@@ -38,7 +38,7 @@ def listar_clientes():
 
 # Função para buscar clientes
 def buscar_cliente():
-    nome_busca = add_busca.get()
+    nome_busca = msg_busca.get()
     encontrados = [cliente for cliente in clientes if nome_busca.lower() in cliente['nome'].lower()]
     
     if encontrados:
@@ -49,10 +49,10 @@ def buscar_cliente():
 
 # Função para agendar horário (a resolver os bugs)
 def agendar_horario():
-    nome = add_agenda_nome.get()
-    data = add_agenda_data.get()
-    horario = add_agenda_horario.get()
-    serviço = add_agenda_servico.get()
+    nome = msg_agenda_nome.get()
+    data = msg_agenda_data.get()
+    horario = msg_agenda_horario.get()
+    serviço = msg_agenda_servico.get()
     
     if nome and data and horario and serviço:
         agendamento = {
@@ -63,10 +63,10 @@ def agendar_horario():
         }
         agendamentos.append(agendamento)
         messagebox.showinfo("Sucesso", f"Agendamento para {nome} no dia {data} às {horario} foi registrado com sucesso! O cliente desejará realizar {serviço}")
-        add_agenda_nome.delete(0, 'end')
-        add_agenda_data.delete(0, 'end')
-        add_agenda_horario.delete(0, 'end')
-        add_agenda_servico.delete(0, 'end')
+        msg_agenda_nome.delete(0, 'end')
+        msg_agenda_data.delete(0, 'end')
+        msg_agenda_horario.delete(0, 'end')
+        msg_agenda_servico.delete(0, 'end')
     else:
         messagebox.showwarning("Erro", "Todos os campos devem ser preenchidos!")
 
@@ -87,7 +87,7 @@ interface.title("Barbearia (Projeto)")
 interface.geometry("1920x1080")
 
 frame = ctk.CTkFrame(interface)
-frame.pack(pady=20, padx=20, fill="both", expand=True)
+frame.pack(pady=2, padx=10, fill="both", expand=True)
 
 adicionar_titulo = ctk.CTkLabel(frame, text="Gerenciamento de barbearia (Teste)", font=("Calisto MT", 24))
 adicionar_titulo.pack(pady=12, padx=10)
@@ -95,23 +95,23 @@ adicionar_titulo.pack(pady=12, padx=10)
 # Configurações para adicionar cliente no sistema
 adicionar_nome = ctk.CTkLabel(frame, text="Nome")
 adicionar_nome.pack()
-add_nome = ctk.CTkEntry(frame)
-add_nome.pack(pady=2)
+msg_nome = ctk.CTkEntry(frame)
+msg_nome.pack(pady=2)
 
 adicionar_sobrenome = ctk.CTkLabel(frame, text="Sobrenome")
 adicionar_sobrenome.pack()
-add_sobrenome = ctk.CTkEntry(frame)
-add_sobrenome.pack(pady=2)
+msg_sobrenome = ctk.CTkEntry(frame)
+msg_sobrenome.pack(pady=2)
 
 adicionar_telefone = ctk.CTkLabel(frame, text="Telefone")
 adicionar_telefone.pack()
-add_telefone = ctk.CTkEntry(frame)
-add_telefone.pack(pady=2)
+msg_telefone = ctk.CTkEntry(frame)
+msg_telefone.pack(pady=2)
 
 adicionar_email = ctk.CTkLabel(frame, text="E-mail")
 adicionar_email.pack()
-add_email = ctk.CTkEntry(frame)
-add_email.pack(pady=2)
+msg_email = ctk.CTkEntry(frame)
+msg_email.pack(pady=2)
 
 botão_adicionar = ctk.CTkButton(frame, text="Adicionar Cliente", command=adicionar_cliente)
 botão_adicionar.pack(pady=2)
@@ -119,8 +119,8 @@ botão_adicionar.pack(pady=2)
 # Buscar clientes cadastrados
 label_busca = ctk.CTkLabel(frame, text="Buscar Cliente por Nome")
 label_busca.pack()
-add_busca = ctk.CTkEntry(frame)
-add_busca.pack(pady=2)
+msg_busca = ctk.CTkEntry(frame)
+msg_busca.pack(pady=2)
 
 botão_buscar = ctk.CTkButton(frame, text="Buscar Cliente", command=buscar_cliente)
 botão_buscar.pack(pady=2)
@@ -128,23 +128,23 @@ botão_buscar.pack(pady=2)
 # Agendamento de horários
 label_agenda_nome = ctk.CTkLabel(frame, text="Nome do Cliente")
 label_agenda_nome.pack()
-add_agenda_nome = ctk.CTkEntry(frame)
-add_agenda_nome.pack(pady=2)
+msg_agenda_nome = ctk.CTkEntry(frame)
+msg_agenda_nome.pack(pady=2)
 
 label_agenda_data = ctk.CTkLabel(frame, text="Data")
 label_agenda_data.pack()
-add_agenda_data = ctk.CTkEntry(frame)
-add_agenda_data.pack(pady=2)
+msg_agenda_data = ctk.CTkEntry(frame)
+msg_agenda_data.pack(pady=2)
 
 label_agenda_horario = ctk.CTkLabel(frame, text="Horário")
 label_agenda_horario.pack()
-add_agenda_horario = ctk.CTkEntry(frame)
-add_agenda_horario.pack(pady=2)
+msg_agenda_horario = ctk.CTkEntry(frame)
+msg_agenda_horario.pack(pady=2)
 
 label_serviço = ctk.CTkLabel(frame, text="Serviço")
 label_serviço.pack()
-add_agenda_servico = ctk.CTkEntry(frame)
-add_agenda_servico.pack(pady=2)
+msg_agenda_servico = ctk.CTkEntry(frame)
+msg_agenda_servico.pack(pady=2)
 
 botão_agendar = ctk.CTkButton(frame, text="Agendar Horário", command=agendar_horario)
 botão_agendar.pack(pady=2)
