0196bdd1350271a84a9bf77f67f9cfb7feb4a3d1
viniscastro <vinislad17@gmail.com>
2025-02-12 15:31:13 -0300

    Update Protótipo.py
    Adição de mais alguns comentários e implementação de um banco de dados

--- Protótipo.py
+++ Protótipo.py
@@ -1,14 +1,18 @@
 import customtkinter as ctk
 from tkinter import messagebox
 
+import sqlite3
+
 # Listas para guardar nome dos clientes e os agendamentos
 
 clientes = []
 agendamentos = []
 
 # Função para adicionar clientes ao sistema da barbearia
-
 def adicionar_cliente():
+    
+    # uso do get() para obter o valor de uma chave, se não houver, retorna "none"
+    # o get() retona o valor de um item de uma chave especificada
     nome = msg_nome.get()
     sobrenome = msg_sobrenome.get()
     telefone = msg_telefone.get()
@@ -22,32 +26,62 @@ def adicionar_cliente():
             "e-mail": email
         }
         clientes.append(cliente)
+        
+        # mensagem que aparece após o registro do cliente
         messagebox.showinfo("Sucesso", f"Cliente {nome} adicionado com sucesso! Seja bem-vindo!")
     else:
+        
+        # mensagem que aparece caso o cliente não preencha todos os campos
         messagebox.showwarning("Erro", "Todos os campos devem ser preenchidos!")
+        
+bancoDados = sqlite3.connect('barbearia.db')
 
-# Função para listar os clientes dentro do prog
+# recebe o objeto onde criei o banco e usar o método cursor, que é o cursor que conseguimos usar os comandos do sql
+cursor = bancoDados.cursor ()
+
+cursor.execute ("""CREATE TABLE IF NOT EXISTS Usuarios
+(
+    id_cliente INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL,
+    telefone TEXT UNIQUE NOT NULL,
+    email TEXT UNIQUE,
+    data_cadastro DATE DEFAULT (DATE('now'))
+);
+""")
+
+# Função para listar os clientes registrados no programa
 
 def listar_clientes():
     if not clientes:
+        
+        # mensagem que aparece caso não haja clientes registrados no programa
         messagebox.showinfo("Clientes", "Nenhum cliente registrado ainda.")
     else:
+        
+        # mensagem informada quando já há clientes registrados, informando o nome, sobrenome, telefone e e-mail
         lista_clientes = "\n".join([f"{cliente['nome']} {cliente['sobrenome']} - {cliente['telefone']} - {cliente['e-mail']}" for cliente in clientes])
         messagebox.showinfo("Lista de Clientes", lista_clientes)
 
 # Função para buscar clientes
 
 def buscar_cliente():
+    
+    # uso do get() para buscar um cliente específico
     nome_busca = msg_busca.get()
     encontrados = [cliente for cliente in clientes if nome_busca.lower() in cliente['nome'].lower()]
     
     if encontrados:
+        
+        # caso haja um cliente com o nome procurado será mostrado uma lista com os clientes com o nome informado
         lista_clientes = "\n".join([f"{cliente['nome']} {cliente['sobrenome']} - {cliente['telefone']} - {cliente['e-mail']}" for cliente in encontrados])
         messagebox.showinfo("Clientes Encontrados", lista_clientes)
     else:
+        
+        # se não houver nenhum cliente com o nome informado, essa mensagem será impressa na tela
         messagebox.showinfo("Clientes Encontrados", "Nenhum cliente encontrado com esse nome.")
 
 # Função para agendar horário (a resolver os bugs)
+
 
 def agendar_horario():
     nome = msg_agenda_nome.get()
@@ -62,17 +96,27 @@ def agendar_horario():
             "horario": horario,
             "serviço": serviço
         }
+        
+        # adição de clientes a lista de agendamento
         agendamentos.append(agendamento)
+        
+        # mensagem que aparece após o agendamento do cliente
         messagebox.showinfo("Sucesso", f"Agendamento para {nome} no dia {data} às {horario} foi registrado com sucesso! O cliente desejará realizar {serviço}")
     else:
+        
+        # mensagem que aparece caso o cliente não preencha todos os campos
         messagebox.showwarning("Erro", "Todos os campos devem ser preenchidos!")
 
 # Função para listar agendamentos (messagebox é mais fácil para exibir as informações, além de mais harmônico para a interface)
 
 def listar_agendamentos():
     if not agendamentos:
+        
+        # mensagem que é informada, caso não haja agendamentos no momento
         messagebox.showinfo("Agendamentos", "Nenhum agendamento registrado ainda.")
     else:
+        
+        # caso contrário, se tiver agendamentos, é informado o nome do cliente, a data e o horário em que ele vai realizar o serviço
         lista_agendamentos = "\n".join([f"{agendamento['nome']} - {agendamento['data']} às {agendamento['horario']}" for agendamento in agendamentos])
         messagebox.showinfo("Lista de Agendamentos", lista_agendamentos)
 
